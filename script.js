dc9af1290dbfc9e3bd8e244ea5e2dc0f71d165d3
sundutta71-rgb <sundutta71@gmail.com>
2026-09-12 21:24:58 +0530

    added mucic player funtionality

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Welcome to Musico")
+console.log("Welcome to Musico");
+// Initialize the Variables
+let songIndex = 0;
+let audioElement = new Audio('1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+
+let songs = [
+    {songName: "Saaiyan", filePath: "song/1.mp3", coverpath: "covers/1.jpg" },
+    {songName: "Saaiyan", filePath: "song/1.mp3", coverpath: "covers/1.jpg" },
+    {songName: "Saaiyan", filePath: "song/1.mp3", coverpath: "covers/1.jpg" },
+    {songName: "Saaiyan", filePath: "song/1.mp3", coverpath: "covers/1.jpg" },
+    {songName: "Saaiyan", filePath: "song/1.mp3", coverpath: "covers/1.jpg" },
+    {songName: "Saaiyan", filePath: "song/1.mp3", coverpath: "covers/1.jpg" },
+]
+//audioElement.play();
+
+//Listen to Events
+myProgressBar.addEventListener('timeupdate',()=>{
+    console.log('timeupdate')
+})
